107aec71f66deb12654d3a21fb4fab7dcde658ef
man <man@DESKTOP-P18ND7P>
2024-11-13 08:41:53 +0800

    添加项目文件。

---
+++ P11_12_2/Distance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11_12_2
+{
+    internal class Distance
+    {
+        private double tiem;
+        private double x;
+        public Distance() { }
+
+        
+        public Distance(double x, double tiem)
+        {
+            this.x = x;
+            this.tiem = tiem;
+            Console.WriteLine("总距离为："+x * tiem);
+        }
+
+        public double suduFun(double x, double y)
+        {
+           
+            return x * y;
+        }
+          public double GetTime()
+            {
+                return tiem;
+            }
+
+           public double GetX()
+            {
+                return x;
+            }
+        public void SetX(double x) {
+            this.x = x;
+        }
+        public void SetTime(double tiem)
+        {
+            this.tiem = tiem;
+        }
+
+    }
+}
---
+++ P11_12_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using P11_12_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11_12_2
+{
+
+    
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        double x = 10100;
+
+      
+        double time = 2;
+        Distance s1 = new Distance();
+
+        s1.SetTime(time);
+        s1.SetX(x);
+        Console.WriteLine("距离为："+s1.suduFun(x, time));
+
+
+        Distance s2 = new Distance(10000,2);
+
+
+        Console.ReadKey();
+        }
+
+       
+    }
